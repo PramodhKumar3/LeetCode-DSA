9b343c08667d51fab8ef90337f333f25fc5ec316
Pramodh Kumar Tamminaina <117188692+PramodhKumar3@users.noreply.github.com>
2025-10-27 12:50:51 +0530

    Time: 16 ms (21.24%), Space: 44.5 MB (53.44%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+class Solution {
+    public static Map<Character, Integer> getStringFrequency(String str){
+        Map<Character, Integer> m1 = new HashMap<>();
+        for(char c : str.toCharArray()){
+            if(! m1.keySet().contains(c))
+                m1.put(c, 1);
+            else
+                m1.put(c, m1.get(c)+1);
+        }
+        return m1;
+    }
+    public boolean isAnagram(String s, String t) {
+        if(!getStringFrequency(s).equals(getStringFrequency(t)))
+            return false;
+        return true;
+    }
+}
