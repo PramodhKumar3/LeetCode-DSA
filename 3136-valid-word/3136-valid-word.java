ccb7e4ce92d34f171707ad5ae3aec0fb4f511b23
Pramodh Kumar Tamminaina <117188692+PramodhKumar3@users.noreply.github.com>
2025-12-08 10:50:12 +0530

    Time: 13 ms (6.08%), Space: 45.9 MB (5.1%) - LeetHub

--- 3136-valid-word/3136-valid-word.java
+++ 3136-valid-word/3136-valid-word.java
@@ -3,3 +3,8 @@ class Solution {
         return word.matches("(?i)(?=.*[b-df-hj-np-tv-z])(?=.*[aeiou])^[a-z0-9]{3,}$");
     }
 }
+
+// (?i)                     -> Case insensitive flag
+// (?=.*[b-df-hj-np-tv-z])  -> atleast one constant
+// (?=.*[aeiou])            -> atleast one vowel
+// ^[a-z0-9]{3,}$           -> Only Alphanumeric, Minimum 3 characters
