32c5da8a3682fb41463cfa4f5564cf5b153cc31a
Pramodh Kumar Tamminaina <117188692+PramodhKumar3@users.noreply.github.com>
2026-02-01 20:37:09 +0530

    Time: 0 ms (100%), Space: 43.7 MB (71.39%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.java
+++ 0088-merge-sorted-array/0088-merge-sorted-array.java
@@ -1,19 +1,21 @@
 class Solution {
-    // public void merge(int[] nums1, int m, int[] nums2, int n) {
-    //     int i = m - 1;
-    //     int j = n - 1;
-    //     int k = m + n - 1;
-    //     while(j >= 0){
-    //         if(i >= 0 && nums1[i] > nums2[j])
-    //             nums1[k--] = nums1[i--];
-    //         else
-    //             nums1[k--] = nums2[j--];
-    //     }
-    // }
-
+// First method O(m+n)
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        for(int j = 0, i = m; j < n; j++, i++)
-            nums1[i] = nums2[j];
-        Arrays.sort(nums1);
+        int i = m - 1;
+        int j = n - 1;
+        int k = m + n - 1;
+        while(j >= 0){
+            if(i >= 0 && nums1[i] > nums2[j])
+                nums1[k--] = nums1[i--];
+            else
+                nums1[k--] = nums2[j--];
+        }
     }
+
+// Second Method O((m+n)log(m+n))
+    // public void merge(int[] nums1, int m, int[] nums2, int n) {
+    //     for(int j = 0, i = m; j < n; j++, i++)
+    //         nums1[i] = nums2[j];
+    //     Arrays.sort(nums1);
+    // }
 }
