353fd74398110015905c18c5ab04faed1df51cd7
Pramodh Kumar Tamminaina <117188692+PramodhKumar3@users.noreply.github.com>
2026-02-11 15:42:09 +0530

    Time: 25 ms (46.23%), Space: 49 MB (46.9%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
@@ -1,17 +1,17 @@
 import java.util.*;
 class Solution {
-    public int subarraySum(int[] nums, int k){
-        int maxlen = 0;
-        for(int i = 0; i < nums.length; i++){
-            int sum = 0;
-            for(int j = i; j < nums.length; j++){
-                sum += nums[j];
-                if(sum == k)
-                    maxlen++;
-            }
-        }
-        return maxlen;
-    }
+    // public int subarraySum(int[] nums, int k){
+    //     int maxlen = 0;
+    //     for(int i = 0; i < nums.length; i++){
+    //         int sum = 0;
+    //         for(int j = i; j < nums.length; j++){
+    //             sum += nums[j];
+    //             if(sum == k)
+    //                 maxlen++;
+    //         }
+    //     }
+    //     return maxlen;
+    // }
 
     // public int subarraySum(int[] nums, int k) {
     //     int maxlen = 0;
@@ -32,21 +32,18 @@ class Solution {
     //     return maxlen;
     // }
 
-    // public int subarraySum(int[] nums, int k) {
-    //     int maxlen = 0;
-    //     int sum = 0;
-    //     Map<Integer, Integer> map = new HashMap<>();
-    //     for(int i = 0; i < nums.length; i++){
-    //         sum += nums[i];
-    //         if(sum == k)
-    //             maxlen = i + 1;
-    //         else{
-    //             if(map.containsKey(sum))
-    //                 maxlen = Math.max(maxlen, i - map.get(sum));
-    //             else
-    //                 map.put(sum, i);
-    //         }
-    //     }
-    //     return maxlen;
-    // }
+    public int subarraySum(int[] nums, int k) {
+        int maxlen = 0;
+        int sum = 0;
+        Map<Integer, Integer> map = new HashMap<>();
+        for(int i = 0; i < nums.length; i++){
+            sum += nums[i];
+            if(sum == k)
+                maxlen++;
+            if(map.containsKey(sum - k))
+                maxlen += map.get(sum - k);
+            map.put(sum, map.getOrDefault(sum, 0) + 1);
+        }
+        return maxlen;
+    }
 }
