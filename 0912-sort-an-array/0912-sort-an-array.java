95a545112730773b3c3c4c4922aebbb7417fe05c
Pramodh Kumar Tamminaina <117188692+PramodhKumar3@users.noreply.github.com>
2026-02-13 15:00:58 +0530

    Time: 65 ms (20.29%), Space: 75 MB (94.33%) - LeetHub

---
+++ 0912-sort-an-array/0912-sort-an-array.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+class Solution {
+    // Merge Sort -> O(N log N)
+    public void merge(int[] nums, int low, int mid, int high){
+        int left = low, right = mid + 1;
+        List<Integer> list = new ArrayList<>();
+        while(left <= mid && right <= high){
+            if(nums[left] <= nums[right])
+                list.add(nums[left++]);
+            else
+                list.add(nums[right++]);
+        }
+        while(left <= mid)
+            list.add(nums[left++]);
+        while(right <= high)
+            list.add(nums[right++]);
+        for(int i = low; i <= high; i++)
+            nums[i] = list.get(i - low);
+    }
+    public void mergesort(int[] nums, int low, int high){
+        if(low >= high)
+            return;
+        int mid = (low + high) / 2;
+        mergesort(nums, low, mid);
+        mergesort(nums, mid + 1, high);
+        merge(nums, low, mid, high);
+    }
+    public int[] sortArray(int[] nums) {
+        mergesort(nums, 0, nums.length - 1);
+        return nums;
+    }
+}
