4f637501fed1c712a984f0fb418d7e526fd9231f
Pramodh Kumar Tamminaina <117188692+PramodhKumar3@users.noreply.github.com>
2026-02-17 11:26:57 +0530

    Time: 2 ms (71.04%), Space: 55.8 MB (31.34%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,25 +1,47 @@
-class Solution {
-    public int majorityElement(int[] nums) {
-    // Sorting Solution
-    /*
-        Arrays.sort(nums);
-        int n = nums.length;
-        return nums[n / 2];
-    */
-    
-    // Solution using HashMap
-        HashMap<Integer, Integer> map = new HashMap<>();
-        for(int num : nums){
-            if(!map.keySet().contains(num))
-                map.put(num, 1);
-            else
-                map.put(num, map.get(num) + 1);
+// import java.util.*;
+// class Solution {
+// //  Better Solution O(N) + O(N log N)
+//     public int majorityElement(int[] nums) {
+//         Map<Integer, Integer> map = new HashMap<>();
+//         for(int i : nums){
+//             if(!map.keySet().contains(i))
+//                 map.put(i, 1);
+//             else
+//                 map.put(i, map.get(i) + 1);
+//         }
+//         for(Map.Entry<Integer, Integer> entry : map.entrySet()){
+//             if(entry.getValue() > nums.length / 2)
+//                 return entry.getKey();
+//         }
+//         return -1;
+//     }
+// }
+
+class Solution{
+//  Optimal Solution
+    public int majorityElement(int[] nums){
+        // 1. Applying Moore's Voting Algorithm
+        int cnt = 0, ele = 0;
+        for(int i = 0; i < nums.length; i++){
+            if(cnt == 0) {
+                cnt = 1;
+                ele = nums[i];
+            } else if(ele == nums[i]) {
+                cnt++;
+            } else {
+                cnt--;
+            }
         }
-        int n = (nums.length) / 2;
-        for(Map.Entry<Integer, Integer> entry : map.entrySet()){
-            if(entry.getValue() > n)
-                return entry.getKey();
+        // 2. Verifying if the element is majority or not
+        int cnt1 = 0;
+        for(int i = 0; i < nums.length; i++){
+            if(nums[i] == ele){
+                cnt1++;
+            }
         }
-        return 0;
+        // 3. Returning majority element
+        if(cnt1 > nums.length / 2)
+            return ele;
+        return -1;
     }
 }
