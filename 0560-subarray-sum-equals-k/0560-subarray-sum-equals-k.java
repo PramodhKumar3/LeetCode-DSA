db4e09e582fca5e3d7af58d9807b9cd84471c788
Pramodh Kumar Tamminaina <117188692+PramodhKumar3@users.noreply.github.com>
2026-03-03 15:04:56 +0530

    Time: 27 ms (29.52%), Space: 49.2 MB (11.95%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
@@ -1,5 +1,6 @@
 import java.util.*;
 class Solution {
+// The below two codes are for longestSubArray sum equals to K codes
 //  Brute Solution
     // public int subarraySum(int[] nums, int k){
     //     int maxlen = 0;
@@ -15,18 +16,33 @@ class Solution {
     // }
 
 //  Optimal Solution
+    // public int subarraySum(int[] nums, int k) {
+    //     int maxlen = 0;
+    //     int sum = 0;
+    //     Map<Integer, Integer> map = new HashMap<>();
+    //     for(int i = 0; i < nums.length; i++){
+    //         sum += nums[i];
+    //         if(sum == k)
+    //             maxlen++;
+    //         if(map.containsKey(sum - k))
+    //             maxlen += map.get(sum - k);
+    //         map.put(sum, map.getOrDefault(sum, 0) + 1);
+    //     }
+    //     return maxlen;
+    // }
+
+// The below is the code for count of sub Arrays sum equals to K
     public int subarraySum(int[] nums, int k) {
-        int maxlen = 0;
-        int sum = 0;
         Map<Integer, Integer> map = new HashMap<>();
+        int sum = 0;
+        int count = 0;
+        map.put(0, 1);
         for(int i = 0; i < nums.length; i++){
             sum += nums[i];
-            if(sum == k)
-                maxlen++;
             if(map.containsKey(sum - k))
-                maxlen += map.get(sum - k);
+                count += map.get(sum - k);
             map.put(sum, map.getOrDefault(sum, 0) + 1);
         }
-        return maxlen;
+        return count;
     }
 }
