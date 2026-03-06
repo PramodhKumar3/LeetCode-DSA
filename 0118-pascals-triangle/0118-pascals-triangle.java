945924bd768b27dda277cba537c20646bf7ce75d
Pramodh Kumar Tamminaina <117188692+PramodhKumar3@users.noreply.github.com>
2026-03-06 15:12:54 +0530

    Time: 0 ms (100%), Space: 43.4 MB (70.9%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.java
+++ 0118-pascals-triangle/0118-pascals-triangle.java
@@ -1,4 +1,5 @@
 import java.util.*;
+// Optimal Solution for Pascal's Trianlge
 class Solution {
     public List<Integer> generateRow(int row){
         long ans = 1;
@@ -18,3 +19,21 @@ class Solution {
         return res;
     }
 }
+// Second Solution
+// class Solution {
+//     public List<List<Integer>> generate(int numRows) {
+//         List<List<Integer>> triangle = new ArrayList<>();
+//         triangle.add(Arrays.asList(1));
+//         for(int i = 1; i < numRows; i++){
+//             List<Integer> row = new ArrayList<>();
+//             List<Integer> prev = triangle.get(i - 1);
+//             row.add(1);
+//             for(int j = 1; j < i; j++){
+//                 row.add(prev.get(j - 1) + prev.get(j));
+//             }
+//             row.add(1);
+//             triangle.add(row);
+//         }
+//         return triangle;
+//     }
+// }
