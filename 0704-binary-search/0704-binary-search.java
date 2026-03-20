a0d60529528edbf5eacf12d04ac8b86ebfd2e138
Pramodh Kumar Tamminaina <117188692+PramodhKumar3@users.noreply.github.com>
2026-03-20 15:40:01 +0530

    Time: 0 ms (100%), Space: 48.7 MB (10.19%) - LeetHub

--- 0704-binary-search/0704-binary-search.java
+++ 0704-binary-search/0704-binary-search.java
@@ -1,9 +1,27 @@
 // Brute Solution -- O(N)
+// class Solution {
+//     public int search(int[] nums, int target) {
+//         for(int i = 0; i < nums.length; i++){
+//             if(nums[i] == target)
+//                 return i;
+//         }
+//         return -1;
+//     }
+// }
+
+// Optimal Solution -- O(log N)
 class Solution {
     public int search(int[] nums, int target) {
-        for(int i = 0; i < nums.length; i++){
-            if(nums[i] == target)
-                return i;
+        int low = 0;
+        int high = nums.length - 1;
+        while(low <= high){
+            int mid = (low + high) / 2;
+            if(nums[mid] == target)
+                return mid;
+            else if(target > nums[mid])
+                low = mid + 1;
+            else
+                high = mid - 1;
         }
         return -1;
     }
